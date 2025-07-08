6f055c2b6259c47c1ad01a64449784b38fd2e408
Juan Pablo Rea Cano <86396@alumnos.utleon.edu.mx>
2025-07-08 13:44:48 -0600

    Se agregaron mas cards

--- frontend/components/Servicios.jsx
+++ frontend/components/Servicios.jsx
@@ -1,8 +1,11 @@
 import React, { useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import servicioTI from '../assets/ti.png'
-import servicio2 from '../assets/servicioSG.png'
-
+import servicioSistemasSeguridad from '../assets/servicioSG.png'
+import servicioEnergias from '../assets/servicioEnergias.png'
+import servicioGuardiasSeguridad from '../assets/servicioGuardiasS.png'
+import servicioCursos from '../assets/servicioCursos.png'
+import servicioDesarrolloWeb from '../assets/servicioWeb.png'
 import '../styles/Servicios.css'
 
 export default function Servicios() {
@@ -36,6 +39,27 @@ export default function Servicios() {
       <h2 className="servicios-title">Servicios de alta calidad</h2>
 
       <div className="servicios-container">
+
+       <div className="servicio-card" style={{ '--delay': '0.6s' }}>
+          <div
+            className="servicio-image"
+            style={{ backgroundImage: `url(${servicioSistemasSeguridad})` }}
+          >
+            <div className="servicio-overlay" />
+            <div className="servicio-content">
+              <h3>
+                Sistemas de
+                <br />
+                seguridad
+              </h3>
+              <span className="underline" />
+              <Link to="/servicios-page" className="servicio-button">
+                Ver más
+              </Link>
+            </div>
+          </div>
+        </div>
+
         <div className="servicio-card" style={{ '--delay': '0.3s' }}>
           <div
             className="servicio-image"
@@ -56,17 +80,80 @@ export default function Servicios() {
           </div>
         </div>
 
+       
+        
+        
         <div className="servicio-card" style={{ '--delay': '0.6s' }}>
           <div
             className="servicio-image"
-            style={{ backgroundImage: `url(${servicio2})` }}
+            style={{ backgroundImage: `url(${servicioEnergias})` }}
           >
             <div className="servicio-overlay" />
             <div className="servicio-content">
               <h3>
-                Sistemas de
+                Energias e
+                <br />
+                innovación
+              </h3>
+              <span className="underline" />
+              <Link to="/servicios-page" className="servicio-button">
+                Ver más
+              </Link>
+            </div>
+          </div>
+          
+          
+        </div>
+        <div className="servicio-card" style={{ '--delay': '0.3s' }}>
+          <div
+            className="servicio-image"
+            style={{ backgroundImage: `url(${servicioGuardiasSeguridad})` }}
+          >
+            <div className="servicio-overlay" />
+            <div className="servicio-content">
+              <h3>
+                Guardias de
                 <br />
                 seguridad
+              </h3>
+              <span className="underline" />
+              <Link to="/servicios-page" className="servicio-button">
+                Ver más
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="servicio-card" style={{ '--delay': '0.3s' }}>
+          <div
+            className="servicio-image"
+            style={{ backgroundImage: `url(${servicioCursos})` }}
+          >
+            <div className="servicio-overlay" />
+            <div className="servicio-content">
+              <h3>
+                Cursos, talleres y
+                <br />
+                capacitaciones
+              </h3>
+              <span className="underline" />
+              <Link to="/servicios-page" className="servicio-button">
+                Ver más
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="servicio-card" style={{ '--delay': '0.3s' }}>
+          <div
+            className="servicio-image"
+            style={{ backgroundImage: `url(${servicioDesarrolloWeb})` }}
+          >
+            <div className="servicio-overlay" />
+            <div className="servicio-content">
+              <h3>
+                Desarrollo de software,
+                <br />
+                web y aplicativos
               </h3>
               <span className="underline" />
               <Link to="/servicios-page" className="servicio-button">
